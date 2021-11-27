f518c6491a506d73bfe96559bf58e076715685ac
Luis <lfponton@gmail.com>
2021-11-27 15:36:08 +0100

    model update orderItem

--- WebClient/Models/OrderItem.cs
+++ WebClient/Models/OrderItem.cs
@@ -2,8 +2,9 @@
 {
     public class OrderItem
     {
-        public long OrderItemId { get; set; }
+        public long OrderId { get; set; }
         public Order Order { get; set; }
+        public long MenuId { get; set; }
         public Menu Menu { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
