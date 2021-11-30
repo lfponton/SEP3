3933c307f2a68b5555448a248285f9b260bcf04a
2945245 <87025282+2945245@users.noreply.github.com>
2021-11-30 09:39:14 +0100

    Changes from Pending to View menus and menuItems

--- WebClient/Models/Menu.cs
+++ WebClient/Models/Menu.cs
@@ -11,7 +11,6 @@ namespace WebClient.Models
         public string Name { get; set; }
         public int Quantity { get; set; }
         public string Type { get; set; }
-        public static string Status { get; set; }
         
         public decimal Price { get; set; }
         public string Description { get; set; }
