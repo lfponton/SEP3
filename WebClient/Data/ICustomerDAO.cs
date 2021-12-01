327fe24216501d927f2647e1381f28a5550d0b13
Marwa93mira <75796895+Marwa93mira@users.noreply.github.com>
2021-12-01 18:12:10 +0100

    Account page

--- WebClient/Data/ICustomerDAO.cs
+++ WebClient/Data/ICustomerDAO.cs
@@ -6,6 +6,7 @@ using WebClient.Models;
 namespace WebClient.Data {
     public interface ICustomerDAO {
         Task<Customer> AddCustomerAsync(Customer customer);
+        Task updateCustomerAsync(string email,Customer customer);
         Task<Customer> GetCustomerAsync(string email, string password);
     }
 }
