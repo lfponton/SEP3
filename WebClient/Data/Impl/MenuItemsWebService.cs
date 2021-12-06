58606c38ecea7968322e030d321b8f57b80a5c7d
2945245 <87025282+2945245@users.noreply.github.com>
2021-12-06 12:07:40 +0100

    Further about creating menus and menulist

--- WebClient/Data/Impl/MenuItemsWebService.cs
+++ WebClient/Data/Impl/MenuItemsWebService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net.Http;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using WebClient.Models;
@@ -31,6 +32,32 @@ namespace WebClient.Data.Impl
             List<MenuItem> menus = JsonSerializer.Deserialize<List<MenuItem>>(result, options);
             return menus;
         }
-        
+
+        public Task<IList<DataServer.Models.MenuItem>> GetMenuItem(int menuId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<DataServer.Models.MenuItem> CreateMenuAsync(DataServer.Models.MenuItem menuItem)
+        {
+            string menuItemAsJson = JsonSerializer.Serialize(menuItem, options);
+            HttpContent content = new StringContent(menuItemAsJson, Encoding.UTF8, "application/json");
+            HttpResponseMessage response = await client.PostAsync("http://localhost:8080/menuItems", content);
+            if (response.IsSuccessStatusCode)
+            {
+                string menuItemAsJsonResponse = await response.Content.ReadAsStringAsync();
+                MenuItem resultMenuItem = JsonSerializer.Deserialize<MenuItem>(menuItemAsJsonResponse, options);
+                return menuItem;
+            }
+
+            throw new Exception($"Error,{response.StatusCode},{response.ReasonPhrase}");
+
+        }
+
+        public Task DeleteMenuItem(long menuItemId)
+        {
+            throw new NotImplementedException();
+        }
+       
     }
 }
