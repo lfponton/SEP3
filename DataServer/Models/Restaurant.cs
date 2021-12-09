d5a11004a240cd32df77c37583423215dddad82b
Agostina-mr <agosmez@gmail.com>
2021-12-09 14:38:15 +0100

    Restaurant.cs

--- DataServer/Models/Restaurant.cs
+++ DataServer/Models/Restaurant.cs
@@ -4,6 +4,7 @@ namespace DataServer.Models
 {
     public class Restaurant
     {
+        public long RestaurantId { get; set; }
         public int Capacity { get; set; }
         public List<Table> Tables { get; set; }
 
