a07a532b03167928e51e84e92bf1434039f48a2c
Agostina-mr <agosmez@gmail.com>
2021-12-10 11:47:38 +0100

    Restaurant tier3

---
+++ DataServer/DataAccess/IRestaurantRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using DataServer.Models;
+
+namespace DataServer.DataAccess.Impl
+{
+    public interface IRestaurantRepository
+    {
+        Task<Restaurant> GetRestaurantAsync();
+    }
+}
---
+++ DataServer/DataAccess/Impl/RestaurantRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using DataServer.Models;
+using DataServer.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataServer.DataAccess.Impl
+{
+    public class RestaurantRepository : IRestaurantRepository
+    {
+        private RestaurantDbContext context;
+
+        public RestaurantRepository(RestaurantDbContext context)
+        {
+            this.context = context;
+
+        }
+        
+        public async Task<Restaurant> GetRestaurantAsync()
+        {
+            return new Restaurant()
+            {
+                Capacity = 10
+            };
+        }
+    }
+}
